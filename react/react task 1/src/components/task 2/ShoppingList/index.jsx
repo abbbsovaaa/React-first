adc9e119cd1260203b8ada41c615581c17f2717e
abbbsovaaa <bbsvaaa10@gmail.com>
2023-02-05 18:11:07 +0400

    add react

---
+++ react/react task 1/src/components/task 2/ShoppingList/index.jsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css'
+
+const ShoppingList =()=>{
+    return(
+        <>
+        
+        <h1>Shopping List</h1>
+        </>
+    )
+}
+export default ShoppingList;
