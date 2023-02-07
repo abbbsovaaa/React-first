ece988ea83cd45d4bd4a6192a22c1e1b0c554178
abbbsovaaa <bbsvaaa10@gmail.com>
2023-02-07 20:13:03 +0400

    add change

--- react/react task 1/src/components/pages/Sican.jsx
+++ react/react task 1/src/components/pages/Sican.jsx
@@ -1,7 +1,6 @@
 const Sican = () => {
     return (
         <>
-
             < h2 style={{
                 display: "flex",
                 justifyContent: 'space-around',
@@ -10,12 +9,19 @@ const Sican = () => {
             <div>
                 <img style={{
                     display: "flex",
-                    justifyContent: "left",
+                    flexDirection: "row",
+                    flexWrap: "nowrap",
+                    justifyContent: "flex-start",
                     objectFit: "cover",
                     width: 300,
                     height: 300,
                     borderRadius: 300
-                }} src="/sican.jpeg" alt="" /> <i>Siçan qidalanmaq üçüx xüsusi şərtlər seçmir. Onlar qoz-fındıq, bütün meyvə-tərəvəz növlərini yeyirlər. Maraqlıdır ki, siçanların görmə qabiliyyəti zəifdir və bu çatışmazlığı onların kəskin iybilmə və eşitmə qabiliyyətləri kompensasiya edir.
+                }} src="/sican.jpeg" alt="" /> <i style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "nowrap",
+                    justifyContent: "flex-start",
+                }}>Siçan qidalanmaq üçüx xüsusi şərtlər seçmir. Onlar qoz-fındıq, bütün meyvə-tərəvəz növlərini yeyirlər. Maraqlıdır ki, siçanların görmə qabiliyyəti zəifdir və bu çatışmazlığı onların kəskin iybilmə və eşitmə qabiliyyətləri kompensasiya edir.
                     Siçanlarla bağlı mənfi rəy təkcə onların görünüşü ilə deyil, həm də onların vəba və s. yoluxucu xəstəliklərin daşıyıcısı olması ilə bağlıdır. Belə ki, 1337-ci ildə məhz siçanlar Avropada “qara ölüm” adını almış vəba epidemiyasını yaymışdılar. Bir neçə il ərzində Avropa əhalisinin 3/1 hissəsi (20-25 milyon insan) bu xəstəliyin qurbanı olmuşdu. Həmin vaxtdan siçanlara həm də ölüm mənbəyi kimi də baxırdılar.
                     Siçovul kimi oxşar gəmiricidən siçanlar öz ölçüləri ilə seçilir. Siçanların ölçüsü maksimum 15-20 sm-a çatır. Bundan başqa, siçanlar daha kiçik qabaritlərə malik olduğu üçün siçovullar qədər əzələ kütləsinə və gücə malik deyillər.</i>
             </div>
