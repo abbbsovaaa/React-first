4cb33a55c93d978f61cfc56bb78f7711c22e398e
abbbsovaaa <bbsvaaa10@gmail.com>
2023-02-23 16:57:52 +0400

    add Elaqeler

--- react/react task 1/src/pages/Elaqeler/index.jsx
+++ react/react task 1/src/pages/Elaqeler/index.jsx
@@ -1,14 +1,51 @@
-import { Button } from "@mui/material";
+import { Divider, Typography } from "@mui/material";
+import { Box, Container, Stack } from "@mui/system";
+import EmailIcon from '@mui/icons-material/Email';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 
 const Elaqeler = () => {
-return(
-    <>
-  <div>
-    <Button>click me </Button>
-  </div>
-    </>
-)
+  return (
+      <Box
+          sx={{
+              backgroundColor: '#2B343B',
+              '&  p': {
+                  color: 'white'
+              },
+              '& *': {
+                  color: 'white',
+                  textDecoration: 'none'
+              },
+          }}
+      >
+          <Container maxWidth="lg">
+              <Stack
+                  direction={{ xs: 'column', md: "row" }}
+                  justifyContent="space-between"
+                  alignItems='center'
+                  py={2}>
+                  <Stack direction="row" gap={4} mb={{ xs: 1, md: 0 }}>
+                      <Typography component="a" href="https://google.com">Servis məntəqələrimiz</Typography>
+                      <Typography component="a" href="https://google.com">Tez-tez verilən suallar</Typography>
+                  </Stack>
+                  <Stack direction="row" gap={2} flexWrap="wrap">
+                      <Stack direction="row" gap={1}>
+                          <EmailIcon />
+                          <Typography> office@solitonservice.com</Typography>
+                      </Stack>
+                      <Divider sx={{ bgcolor: 'white', width: 2 }} orientation="vertical" flexItem />
+                      <Stack direction="row" gap={1}>
+                          <FacebookIcon />
+                          <InstagramIcon />
+                          <WhatsAppIcon />
+                      </Stack>
+                  </Stack>
+              </Stack>
+          </Container>
+      </Box>
+  )
 }
 
 export default Elaqeler;
